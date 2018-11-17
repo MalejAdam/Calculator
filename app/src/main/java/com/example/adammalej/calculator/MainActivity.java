2fcf6b13e540c2daca509393ce38b8940ce821ec
Adam Malej <adammalej1@gmail.com>
2018-11-17 16:52:31 +0100

    Add method which waiting for click on numbers.

--- app/src/main/java/com/example/adammalej/calculator/MainActivity.java
+++ app/src/main/java/com/example/adammalej/calculator/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.adammalej.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView expression = findViewById(R.id.txtvExpression);
+    Button onClickedButton;
+    int[] numberButtons = {R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numberOnClick();
     }
+
+    private void numberOnClick(){
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickedButton = (Button) v;
+                expression.setText(onClickedButton.getText());
+            }
+        };
+    }
+
+
 }
